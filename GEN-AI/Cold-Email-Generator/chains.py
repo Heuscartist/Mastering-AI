8e2ccc4f450e2cec42a145920548476c3c3707ec
Adil Mubashir Chaudhry <75829014+Heuscartist@users.noreply.github.com>
2025-01-22 19:29:40 +0500

    Cold Email Generator

---
+++ GEN-AI/Cold-Email-Generator/chains.py
@@ -0,0 +1,60 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Adil, an Electrical Engineer Majoring in Artificial Intelligence from LUMS. 
+            You are passionately looking for jobs to enter the tech industry and creating tailored resume for various job postings.
+            Over your academic career you have worked on numerous projects, allowing you to gain valuable understanding of various domains.
+            Your job is to provide a list of projects using the projects listed in the {link_list} ONLY that are related to the job posting given. 
+            The project should be tailored using only the given projects to the post and fulfil the requirements of the employer.
+            You should also generate a cover letter that would be suitable for applying for the position.
+            Do not provide a preamble.
+            
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
